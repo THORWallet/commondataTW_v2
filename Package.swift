97c76288c246cde337a7a6d05862d9d8cbeea745
mattgabriel <matt.g.martire@gmail.com>
2025-03-06 13:22:44 +0100

    update package

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "TWCommonData",
+    name: "commondatatw_v2",
     platforms: [
         .macOS(.v10_15),
         .iOS(.v13),
@@ -14,7 +14,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "commondatatw_v2",
-            targets: ["TWCommonData"]),
+            targets: ["commondatatw_v2"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -24,7 +24,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "TWCommonData",
+            name: "commondatatw_v2",
             dependencies: [.product(name: "SwiftProtobuf", package: "swift-protobuf")],
             path: "Sources/swift" // Specify the path to the swift folder
         ),
